8f61b3f6fea278cfd0b2118a07642ffcf417c0cc
unknown <ddev.chama@gmail.com>
2020-02-17 22:17:54 +0700

    Last Modift to Night [17/02/20]

--- template/header.php
+++ template/header.php
@@ -50,6 +50,9 @@
                         <a class="nav-link" href="#">ติดต่อเรา</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="#">สมัครสมาชิก</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="./administrator/login.php">Admin</a>
                     </li>
                     <li class="pt-2 pl-2">
